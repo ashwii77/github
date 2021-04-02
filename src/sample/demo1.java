f3c8fefb7564c77dde9814bb9bbc6fac4615d775
family <family@ashwini>
2021-04-02 23:10:51 +0530

    [ASHWINI]-ADDING PROJECT TOGITHUB

---
+++ src/sample/demo1.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class demo1 {
+	public static void main(String[] args) {
+		System.out.println("ashwini");
+		System.out.println("khawashi");
+		System.out.println("work with xorient  as aqa");
+	}
+
+}
